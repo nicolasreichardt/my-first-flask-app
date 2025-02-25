01d3891a170955674ed8948be6c533bb0d55a708
Nicolas Reichardt <nicolasreichardt@gmail.com>
2025-02-25 22:53:53 +0100

    Change to About page

--- flaskapp.py
+++ flaskapp.py
@@ -3,22 +3,18 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return f'''
-        <h1>Welcome to the Home Page</h1>
-        <p>Go to the <a href="{url_for('hello')}">Hello Page</a></p>
-        <p>Go to the <a href="{url_for('hello', city='New York')}">Hello New York Page</a></p>
-    '''
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
 @app.route("/hello")
-@app.route("/hello/<city>")
-def hello(city=None):
-    if city is None or city == "":
-        return "<h1>Hello!</h1>"
-    else:
-        return f"<h1>Hello, {city}!</h1>"
-    
+def hello():
+    return render_template('hello.html')
 
+@app.route("/layout")
+def layout():
+    return render_template('layout.html')
+    
 @app.route("/about")
 def about():
     return render_template('about.html')
